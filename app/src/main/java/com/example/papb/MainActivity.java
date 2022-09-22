62cd9603aaf8459839ee3de59d9c7cbebb7eb921
praktikan <prakitan@gmail.com>
2022-09-22 15:33:22 +0700

    first commit

---
+++ app/src/main/java/com/example/papb/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.papb;
+
+import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayoutMediator mediator;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = findViewById(R.id.pager);
+
+        SampleAdapter adapter = new SampleAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(adapter);
+
+        mediator = new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("Tab" + " " + (position + 1));
+            }
+        });
+        mediator.attach();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sample_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_profile:
+                Intent profileInten = new Intent(this, MainActivity2.class);
+                startActivity(profileInten);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
